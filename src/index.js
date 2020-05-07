4f604317e88d7e725db6174099c557a80fc2c465
João Santos <joaorafaelsantos@outlook.pt>
2020-05-07 16:40:43 +0100

    Added traditional animated cube example

--- src/index.js
+++ src/index.js
@@ -1,2 +1,41 @@
 import './styles.css';
-alert('Welcome');
+import * as THREE from 'three';
+
+let camera;
+let scene;
+let renderer;
+let mesh;
+
+function init() {
+  const canvas = document.querySelector('#canvas');
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10,
+  );
+  camera.position.z = 1;
+
+  scene = new THREE.Scene();
+
+  const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+  const material = new THREE.MeshNormalMaterial();
+
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
+
+  renderer.render(scene, camera);
+}
+
+init();
+animate();
